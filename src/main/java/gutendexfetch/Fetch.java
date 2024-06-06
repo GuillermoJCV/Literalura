b09c7616d5a7fef03db55d30d4a2808f57c9f5d0
GuillermoJCV <memo92975@gmail.com>
2024-06-06 15:10:58 -0600

    Hace la peticion y envía un String como respuesta al recurso books siempre que se encuentre un nombre

--- src/main/java/gutendexfetch/Fetch.java
+++ src/main/java/gutendexfetch/Fetch.java
@@ -1,6 +1,8 @@
 package gutendexfetch;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
@@ -10,24 +12,25 @@ import java.net.http.HttpResponse;
 
 public class Fetch {
 	
-	private final String targetURL = "https://gutendex.com";
-	private HttpResponse<String> response;
-	
 	public Fetch() {
 		
 	}
 	
-	private HttpRequest useFetch(String url) throws URISyntaxException{
-		String completeURL = targetURL + url;
-		HttpRequest request = HttpRequest.newBuilder()
-				  .uri(new URI(completeURL))
-				  .GET()
-				  .build();
+	public String getBookByName(String name) throws URISyntaxException, IOException, InterruptedException {
 		
-		return request;
+		HttpResponse<String> response = getResponse("search=", name);
+		return response.body();
 	}
 	
-	public HttpResponse<String> saveResponse(HttpRequest request) throws IOException, InterruptedException {
+	private HttpResponse<String> getResponse(String resource, String itemToAccess) throws URISyntaxException, IOException, InterruptedException {
+		
+		HttpRequest request;
+		request = useFetch(resource, itemToAccess);
+		response = saveResponse(request);
+		return response;
+	}
+	
+	private HttpResponse<String> saveResponse(HttpRequest request) throws IOException, InterruptedException {
 		HttpClient client = HttpClient.newHttpClient();
 		HttpResponse<String> response = client
 				  .send(request, HttpResponse.BodyHandlers.ofString());
@@ -35,10 +38,33 @@ public class Fetch {
 		return response;
 	}
 	
-	public HttpResponse<String> getResponse(String resource) throws URISyntaxException, IOException, InterruptedException {
-		HttpRequest request;
-		request = useFetch(resource);
-		response = saveResponse(request);
-		return response;
+	private HttpRequest useFetch(String url, String itemToAcess) throws URISyntaxException, UnsupportedEncodingException, MalformedURLException{
+		
+		HttpRequest request = HttpRequest.newBuilder()
+				  .uri(useURL(url + itemToAcess))
+				  .GET()
+				  .build();
+		
+		return request;
 	}
+	
+	private URI useURL(String item) throws URISyntaxException, MalformedURLException {
+		URI result;
+		
+		final String scheme = "https";
+		final String host = TARGET_URL;
+		final String path = REST_RESOURCE;
+		final String query = item;
+		
+		//TODO: Contemplar cuando la query está vacía
+		
+		result = new URI(scheme, host, path, query, null);
+		System.out.println(result.toURL());
+		
+		return result;
+	}
+	
+	private final String TARGET_URL = "gutendex.com";
+	private final String REST_RESOURCE = "/books";
+	private HttpResponse<String> response;
 }
