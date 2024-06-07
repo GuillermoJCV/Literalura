2ebf0901e72af7f6e06ed70f15c51aa4dd4bac71
GuillermoJCV <memo92975@gmail.com>
2024-06-06 18:10:37 -0600

    Elimina el fetch de los datos

--- src/main/java/alura/literalura/com/challenge/LiteraluraApplication.java
+++ src/main/java/alura/literalura/com/challenge/LiteraluraApplication.java
@@ -1,30 +1,14 @@
 package alura.literalura.com.challenge;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import gutendexfetch.Fetch;
 
 @SpringBootApplication
 public class LiteraluraApplication {
 	
 	public static void main(String[] args) {
 		
-		Fetch books = new Fetch();
-		
 		SpringApplication.run(LiteraluraApplication.class, args);
-		
-		try {
-			String response = books.getBookByName("dickens great");
-			System.out.println("Response: ");
-			System.out.println(response);
-		} catch (URISyntaxException | IOException | InterruptedException e) {
-			
-			e.printStackTrace();
-		}
 	}
 
 }
