7a2cd7edfd25b51556ce7893b19162d809a1e60a
GuillermoJCV <memo92975@gmail.com>
2024-06-18 19:23:01 -0600

    Añade los repositories para poder ejecutar métodos a la base de datos

--- src/main/java/repositories/BooksRepository.java
+++ src/main/java/repositories/BooksRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import models.BookModel;
 
-public interface BooksRepository extends JpaRepository<BookModel, Integer>{
+public interface BooksRepository extends JpaRepository<BookModel, Integer> {
 
 }
