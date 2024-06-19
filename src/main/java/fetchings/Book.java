bbba55357ef2d3417ddbc4a435aa70d36ad11d4d
GuillermoJCV <memo92975@gmail.com>
2024-06-18 19:22:23 -0600

    Cambia la peticion a gutendex a fetchings para separar la lógica de la app

---
+++ src/main/java/fetchings/Book.java
@@ -0,0 +1,75 @@
+package fetchings;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties({"bookshelves", "formats", "id", "translators"})
+public class Book {
+	
+	public Book() {
+		
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public String[] getLanguages() {
+		return languages;
+	}
+	public Author getAuthor() {
+		return authors[0];
+	}
+	
+	public Author[] getAuthors() {
+		return authors;
+	}
+	
+	public int getDownloadCount() {
+		return download_count;
+	}
+	
+	public String[] getSubjects() {
+		return subjects;
+	}
+	
+	public String getMediaType() {
+		return mediaType;
+	}
+	
+	public boolean isCopyright() {
+		return copyright;
+	}
+	
+	@Override
+	public String toString() {
+		String result = "Title : " + getTitle() + "\n" +
+						"Author : " + getAuthor().getName() + "\n" +
+						"Languages : " + getLanguages()[0] + "\n" +
+						"Download Count : " + getDownloadCount() + "\n" +
+						"<-------------------------------->";
+		
+		return result;
+	}
+	
+	@JsonProperty("copyright")
+	private boolean copyright;
+
+	@JsonProperty("media_type")
+	private String mediaType;
+
+	@JsonProperty("subjects")
+	private String[] subjects;
+
+	@JsonProperty("authors")
+	private Author[] authors;
+
+	@JsonProperty("title")
+	private String title;
+	
+	@JsonProperty("languages")
+	private String[] languages;
+	
+	@JsonProperty("download_count")
+	private int download_count;
+	
+}
---
+++ src/main/java/fetchings/Books.java
@@ -0,0 +1,41 @@
+package fetchings;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+@JsonIgnoreProperties({"next", "previous"})
+public class Books {
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public Book[] getBooks() {
+		return books;
+	}
+	
+	public String getNextPage() {
+		return nextPage;
+	}
+
+	public String getPreviousPage() {
+		return previousPage;
+	}
+	
+	@JsonProperty("count")
+	private int count;
+	
+	@JsonSetter("next")
+	private String nextPage;
+	
+	@JsonSetter("previous")
+	private String previousPage;
+	
+	@JsonSetter("results")
+	private Book[] books;
+
+	public Books() {
+		
+	}
+}
