89741ef6f53e135e9a81a2cd3e5a2fa3e2d13699
GuillermoJCV <memo92975@gmail.com>
2024-06-22 22:40:20 -0600

    Elimina directorios innecesarios y ordena el proyect

--- src/main/java/repositories/BooksRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import models.BookModel;
-
-public interface BooksRepository extends JpaRepository<BookModel, Integer> {
-
-}
