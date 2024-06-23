8c6c2235cb78c5031e5fd4cc3778b5af9c17086a
GuillermoJCV <memo92975@gmail.com>
2024-06-22 22:42:01 -0600

    Configura el proyecto para habilitar los JPARepositories

--- src/main/java/alura/literalura/com/challenge/AppConfig.java
+++ src/main/java/alura/literalura/com/challenge/AppConfig.java
@@ -1,24 +1,10 @@
 package alura.literalura.com.challenge;
 
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import repositories.AuthorRepository;
-import repositories.BooksRepository;
-import services.AuthorService;
-import services.BookService;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories
 public class AppConfig {
-	
-	@Bean
-	BookService bookService(BooksRepository booksRepository) {
-		return new BookService(booksRepository);
-	}
-	
-	@Bean
-	AuthorService authorService(AuthorRepository authorRepository) {
-		return new AuthorService(authorRepository);
-	}
+
 }
